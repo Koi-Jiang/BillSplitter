f8e32eec30d3a1f0bdb3f90c4b52d80b528f82d0
Emily Jiang <brilliantemilyjiang@gmail.com>
2023-12-05 23:32:47 -0500

    chore: add dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ 
-      registerType: 'autoUpdate',
+    VitePWA({
+      registerType: "autoUpdate",
       devOptions: {
-        enabled: true
-      }
+        enabled: true,
+      },
     }),
   ],
-})
+});
