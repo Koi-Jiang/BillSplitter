82e9f12a6b61ffe4d47e5c817c54833e2340f2b0
Emily Jiang <brilliantemilyjiang@gmail.com>
2023-12-06 02:52:53 -0500

    chore: add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import FrontPage from "./components/FrontPage/FrontPage";
+import { createTheme, StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+// font style
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FrontPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 );
