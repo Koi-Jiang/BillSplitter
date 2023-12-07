6098f815434c7e480878b8e8d3d7ea98f9e0852d
Emily Jiang <brilliantemilyjiang@gmail.com>
2023-12-07 07:13:23 -0500

    feat: Finish front page UI

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -1,12 +1,43 @@
-import { Container, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function FrontPage() {
+  const [name, setName] = useState<string>("");
+  const navigate = useNavigate();
+
   return (
-    <Container maxWidth="sm" className="flex flex-col min-h-screen items-center justify-center">
-      <Typography variant="h4" component="h1">
-        Create a Room to Split Your Bill
+    <Container
+      maxWidth="md"
+      className="flex flex-col min-h-screen min-h-[100dvh] items-center justify-center"
+    >
+      <Typography variant="h5" component="h1" gutterBottom>
+        Create a Room
       </Typography>
-      <TextField id="outlined-basic" label="Group Name" variant="standard" margin="normal" />
+      <Box className="flex items-center w-full">
+        <TextField
+          id="outlined-basic"
+          fullWidth
+          variant="standard"
+          margin="none"
+          placeholder="What is it called..?"
+          inputProps={{ maxLength: 50, style: { fontSize: "xx-large" } }}
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyUp={(e) => e.code === "Enter" && navigate(`/${name}`)}
+        />
+        <IconButton color="primary" disabled={name === ""} href={`/${name}`}>
+          <ArrowForwardIcon fontSize="large" />
+        </IconButton>
+      </Box>
     </Container>
   );
 }
