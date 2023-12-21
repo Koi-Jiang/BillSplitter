8f61297d14a7097696dee477c242f48b2d5a384c
Emily Jiang <brilliantemilyjiang@gmail.com>
2023-12-20 23:50:18 -0500

    feat: finish bill detail dialog

--- src/components/BillDetailDialog/BillDetailDialog.tsx
+++ src/components/BillDetailDialog/BillDetailDialog.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
-import { Box, Dialog, DialogContent, Divider, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Divider,
+  IconButton,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { numberFormatter } from "../../utils/numberFormatter";
 import { BillInfo } from "../../utils/BillInfo";
+import Avatar from "boring-avatars";
+import CloseIcon from "@mui/icons-material/Close";
 
 export interface BillEditArgs {
   // billInfo? means billInfo or undefined
@@ -14,33 +25,88 @@ export interface BillEditArgs {
 const BillDetailDialog: FC<BillEditArgs> = ({ billInfo, isOpen, onCancel }) => {
   const { palette } = useTheme();
   return (
-    <Dialog
-      open={isOpen}
-      maxWidth="xs"
-      fullWidth
-      onClose={onCancel}
-    >
-      <DialogContent>
-        <Typography component="h2" variant="h5" className="text-center">
-          {billInfo.description}
-        </Typography>
-        <Typography component="p" variant="subtitle2" className="text-center">
-          Dec. 12, 2024
-        </Typography>
-        <Divider className="my-2"/>
-        <Box component="section">
-          <Typography component="h3" variant="h5">
-            {billInfo.payer}
+    <Dialog open={isOpen} maxWidth="sm" fullWidth onClose={onCancel}>
+      <DialogActions className="absolute right-0">
+        <IconButton onClick={onCancel}>
+          <CloseIcon fontSize="large"/>
+        </IconButton>
+      </DialogActions>
+      <DialogContent className="p-9">
+        <Box component="section" className="mb-8">
+          <Typography
+            component="h2"
+            variant="h4"
+            className="text-center font-normal"
+            gutterBottom
+          >
+            {billInfo.description}
+          </Typography>
+          <Typography component="p" variant="subtitle2" className="text-center">
+            Dec. 12, 2024
           </Typography>
         </Box>
-        <Box component="section">
-          <Typography component="h3" variant="h5">
-            Lenders:
+        <Box component="div" className="my-2 flex flex-row items-center">
+          <Avatar
+            size={48}
+            name={billInfo.payer}
+            variant="beam"
+            colors={["#5b36c2", "#6f53d1", "#866ddf", "#9d85ed", "#b69efa"]}
+          />
+          <Typography
+            component="p"
+            variant="h6"
+            className="flex-auto mx-6 font-normal"
+          >
+            <span className="font-bold">{billInfo.payer}</span> paid
           </Typography>
-          {billInfo.lenders.map((v, i) => (
-            <Typography component="h4" variant="h6" className="font-normal indent-28" key={i}>{v}</Typography>
-          ))}
+          <Typography
+            component="p"
+            variant="h6"
+            color={palette.primary.light}
+            className="flex-initial"
+          >
+            {numberFormatter(billInfo.amount)}
+          </Typography>
         </Box>
+        <Divider className="my-2" />
+        {billInfo.lenders.map((v, i) => {
+          if (v !== billInfo.payer) {
+            return (
+              <Box
+                key={i}
+                component="div"
+                className="my-4 flex flex-row items-center"
+              >
+                <Avatar
+                  size={48}
+                  name={v}
+                  variant="beam"
+                  colors={[
+                    "#5b36c2",
+                    "#6f53d1",
+                    "#866ddf",
+                    "#9d85ed",
+                    "#b69efa",
+                  ]}
+                />
+                <Typography
+                  component="p"
+                  variant="h6"
+                  className="flex-auto mx-6 font-normal"
+                >
+                  <span className="font-bold">{v}</span> owes
+                </Typography>
+                <Typography
+                  component="p"
+                  variant="h6"
+                  className="flex-initial font-normal"
+                >
+                  {numberFormatter(billInfo.amount / billInfo.lenders.length)}
+                </Typography>
+              </Box>
+            );
+          }
+        })}
       </DialogContent>
     </Dialog>
   );
